b716fe56ab3d8925565cae37604490582369fbfa
Kraftsman <shamsasigri@gmail.com>
2024-10-23 22:35:18 +0000

    changed the provider list to display the card items in a grid

--- resources/js/Components/StorageProviders/ProviderList.vue
+++ resources/js/Components/StorageProviders/ProviderList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <div v-for="provider in providers" :key="provider.id" class="mb-4">
             <ProviderListItem :provider="provider" @delete="showModal = true" />
             <ConfirmationModal :show="showModal" @close="showModal = false">
