f2140dee69b1e43d51b75ad994351532ff6d6984
Kraftsman <shamsasigri@gmail.com>
2024-10-26 09:11:10 +0000

    initialized file manager service and created new controller instance

--- app/Http/Controllers/FileManagerController.php
+++ app/Http/Controllers/FileManagerController.php
@@ -3,54 +3,34 @@
 namespace App\Http\Controllers;
 
 use App\Models\File;
-use App\Models\Folder;
+use App\Models\StorageProvider;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+use App\Services\FileManagerService;
+use App\Services\StorageProviderService;
+
 
 use Inertia\Inertia;
 
 class FileManagerController extends Controller 
 {
-    public function index()
+
+    /**
+     * The file manager service.
+     *
+     * @var FileManagerService
+     */
+    protected $fileManagerService;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param FileManagerService $fileManagerService
+     */
+    public function __construct(FileManagerService $fileManagerService)
     {
-        $folders = Folder::all()->map(function ($folder) {
-            return [
-                'id' => $folder->id,
-                'name' => $folder->name,
-                'icon' => $this->getFolderIcon($folder->type),
-                'fileCount' => $folder->files->count(),
-                'size' => $this->formatSize($folder->size),
-            ];
-        });
-
-        $files = File::all()->map(function ($file) {
-            return [
-                'id' => $file->id,
-                'name' => $file->name,
-                'type' => $file->type,
-                'icon' => $this->getFileIcon($file->type),
-                'preview' => $file->preview_url,
-                'size' => $this->formatSize($file->size),
-            ];
-        });
-
-        return Inertia::render('FileManager', [
-            'folders' => $folders,
-            'files' => $files,
-        ]);
-    }
-
-    private function getFolderIcon($type)
-    {
-        // Map folder types to icon components
-    }
-
-    private function getFileIcon($type)
-    {
-        // Map file types to icon components
-    }
-
-    private function formatSize($size)
-    {
-        // Format file size
+        $this->fileManagerService = $fileManagerService;
     }
 
 }
