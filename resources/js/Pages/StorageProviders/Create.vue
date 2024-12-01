aef2007d54631ecf58ab98f96f7753754b8431b0
Kraftsman1 <shamsasigri@gmail.com>
2024-12-01 23:11:00 +0000

    refactor: update Create.vue to use axios for API calls and improve layout structure

--- resources/js/Pages/StorageProviders/Create.vue
+++ resources/js/Pages/StorageProviders/Create.vue
@@ -3,24 +3,28 @@ import AppLayout from '@/Layouts/AppLayout.vue'
 import ProviderForm from '@/Components/StorageProviders/ProviderForm.vue'
 
 const createProvider = (formData) => {
-    $inertia.post(route('storage-providers.store'), formData)
+    axios.post(route('storage-providers.store'), formData);
+
 }
 
 </script>
 <template>
-    <app-layout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Create Storage Provider
-            </h2>
-        </template>
+    <AppLayout>
+        <main class="flex-1 p-8 overflow-auto">
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
-                    <ProviderForm @submitted="createProvider" submit-button-text="Create Provider" />
+            <header>
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    Create Storage Provider
+                </h2>
+            </header>
+
+            <div class="py-12">
+                <div class="mx-auto">
+                    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
+                        <ProviderForm @submitted="createProvider" submit-button-text="Create Provider" />
+                    </div>
                 </div>
             </div>
-        </div>
-    </app-layout>
+        </main>
+    </AppLayout>
 </template>
