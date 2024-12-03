1de638f2c7c34dc1d8dd0be83fb8f2d0ea4477d2
Kraftsman1 <shamsasigri@gmail.com>
2024-12-03 23:15:40 +0000

    refactor: convert ProviderList component to Composition API and simplify delete confirmation logic

--- resources/js/Components/StorageProviders/ProviderList.vue
+++ resources/js/Components/StorageProviders/ProviderList.vue
@@ -1,3 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+import ProviderListItem from './ProviderListItem.vue';
+import ConfirmationModal from '../ConfirmationModal.vue';
+
+const props = defineProps({
+    providers: {
+        type: Array,
+        required: true
+    }
+});
+
+console.log(props.providers)
+
+const showModal = ref(false);
+
+const confirmDelete = () => {
+    showModal.value = false;
+    emit('delete');
+}
+
+</script>
+
 <template>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <div v-for="provider in providers" :key="provider.id" class="mb-4">
@@ -17,32 +40,3 @@
         </div>
     </div>
 </template>
-
-<script>
-import ProviderListItem from './ProviderListItem.vue';
-import ConfirmationModal from '../ConfirmationModal.vue';
-
-export default {
-    components: {
-        ProviderListItem,
-        ConfirmationModal
-    },
-    data() {
-        return {
-            showModal: false,
-        }
-    },
-    methods: {
-        confirmDelete() {
-            this.$emit('delete');
-            this.showModal = false;
-        }
-    },
-    props: {
-        providers: {
-            type: Array,
-            required: true
-        }
-    }
-}
-</script>
